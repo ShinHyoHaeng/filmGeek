31ad1317a0227e7fb20f7437d9a721be96e3339d
Haeng <hyohaeng.shin@gmail.com>
2022-05-25 17:24:46 +0900

    gh-pages 설정 수정

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/ShinHyoHaeng/react-tmdb2notion/'>
+  <BrowserRouter basename='/react-tmdb2notion/'>
     <React.StrictMode>
       <App />
     </React.StrictMode>
