5c11a5c0008e632725d613b1f8acbfc42c5d30c4
Haeng <hyohaeng.shin@gmail.com>
2022-06-09 16:42:45 +0900

    불필요한 파일 삭제

--- src/components/search/GetLanguage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GetLanguage = () => {
-  return (
-    <div>GetLanguage</div>
-  )
-}
-
-export default GetLanguage
--- src/components/search/GetType.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GetType = () => {
-  return (
-    <div>GetType</div>
-  )
-}
-
-export default GetType
